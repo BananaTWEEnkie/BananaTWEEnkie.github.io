c0929714c8aaf051a64bd810fd0ed7bdf0dd2e05
Thuy <tweenguyentran@gmail.com>
2016-05-20 01:14:08 -0700

    Update

--- js/main1.js
+++ js/main1.js
@@ -1,55 +1,9 @@
-function changeTheme() {
-	var theme = document.getElementById("theme").innerHTML;
-	
-	if (theme == "Dark Theme") {
-		document.getElementById("theme").innerHTML = "Light Theme";
-	} else {
-		document.getElementById("theme").innerHTML = "Dark Theme";
-	}
-	
-	$('body').toggleClass('clicked');
-	$('button').toggleClass('clicked2');
-}
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
-$(function() {
-
-    var newHash      = "",
-        $mainContent = $("#main-content"),
-        $pageWrap    = $("#page-wrap"),
-        baseHeight   = 0,
-        $el;
-        
-    $pageWrap.height($pageWrap.height());
-    baseHeight = $pageWrap.height() - $mainContent.height();
-    
-    $("nav").delegate("a", "click", function() {
-        window.location.hash = $(this).attr("href");
-        return false;
-    });
-    
-    $(window).bind('hashchange', function(){
-    
-        newHash = window.location.hash.substring(1);
-        
-        if (newHash) {
-            $mainContent
-                .find("#guts")
-                .fadeOut(200, function() {
-                    $mainContent.hide().load(newHash + " #guts", function() {
-                        $mainContent.fadeIn(200, function() {
-                            $pageWrap.animate({
-                                height: baseHeight + $mainContent.height() + "px"
-                            });
-                        });
-                        $("nav a").removeClass("current");
-                        $("nav a[href='"+newHash+"']").addClass("current");
-                    });
-                });
-        };
-        
-    });
-    
-    $(window).trigger('hashchange');
-
-});
-
+  ga('create', 'UA-76613025-1', 'auto');
+  ga('send', 'pageview');
+  
+  
