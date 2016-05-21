c69daa52efb9538289fcdcd93614bd292b8b7b0f
Thuy <tweenguyentran@gmail.com>
2016-05-20 22:08:13 -0700

    Adjust look

--- js/main.js
+++ js/main.js
@@ -9,47 +9,18 @@ function changeTheme() {
 	
 	$('body').toggleClass('clicked');
 	$('button').toggleClass('clicked2');
+	$('.tooltiptext').toggleClass('clicked3');
 }
 
-$(function() {
-
-    var newHash      = "",
-        $mainContent = $("#main-content"),
-        $pageWrap    = $("#page-wrap"),
-        baseHeight   = 0,
-        $el;
-        
-    $pageWrap.height($pageWrap.height());
-    baseHeight = $pageWrap.height() - $mainContent.height();
-    
-    $("nav").delegate("a", "click", function() {
-        window.location.hash = $(this).attr("href");
-        return false;
-    });
-    
-    $(window).bind('hashchange', function(){
-    
-        newHash = window.location.hash.substring(1);
-        
-        if (newHash) {
-            $mainContent
-                .find("#guts")
-                .fadeOut(200, function() {
-                    $mainContent.hide().load(newHash + " #guts", function() {
-                        $mainContent.fadeIn(200, function() {
-                            $pageWrap.animate({
-                                height: baseHeight + $mainContent.height() + "px"
-                            });
-                        });
-                        $("nav a").removeClass("current");
-                        $("nav a[href='"+newHash+"']").addClass("current");
-                    });
-                });
-        };
-        
-    });
-    
-    $(window).trigger('hashchange');
-
+$(document).ready(function() {
+	$('body').css('display', 'none');
+	$('body').fadeIn(2000);
+	$('.link').click(function() {
+		newLocation = $('.link a').attr("href");
+		$('body').fadeOut(2000, newpage);
+	});
+	function newpage() {
+		window.location = newLocation;
+	}
 });
 
