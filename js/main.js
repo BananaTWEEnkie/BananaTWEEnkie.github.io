ab6bd27356f844b924f3253da4ad22dadce32f7d
Thuy-Tien Nguyen-Tran <tweenguyentran@gmail.com>
2017-06-07 14:09:00 -0700

    added js game

--- js/main.js
+++ js/main.js
@@ -22,6 +22,19 @@ $("#quote").mouseout(function() {
 	});
 });
 
+function changeTheme() {
+	var theme = document.getElementById("theme").innerHTML;
+	
+	if (theme == "Dark Theme") {
+		document.getElementById("theme").innerHTML = "Light Theme";
+	} else {
+		document.getElementById("theme").innerHTML = "Dark Theme";
+	}
+	
+	$('body').toggleClass('clicked');
+	$('button').toggleClass('clicked2');
+	$('.tooltiptext').toggleClass('clicked3');
+}
 
 $(document).ready(function() {
 	$('body').css('display', 'none');
